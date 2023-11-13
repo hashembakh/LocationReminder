2893f79cbe111c9f9642883fe59c036211004bb6
hashem <hashembakh@gmail.com>
2023-11-13 10:32:53 +0300

    second commit

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -16,7 +16,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.context.stopKoin
 import org.koin.dsl.module
-import org.koin.
+import org.koin.test.KoinTest
 import org.koin.*
 
 
